a29f4ced7ae76a38456db45580cbe8bf6ffd6974
Shivani Thakkar <shivaniiithakkar@gmail.com>
2023-02-14 18:29:18 +0530

    Add project files.

---
+++ Controllers/JWTTokenController.cs
@@ -0,0 +1,60 @@
+﻿using DemoJWT.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DemoJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JWTTokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        
+
+        public JWTTokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+           
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult PostUser([FromForm] UserRequest userRequest) 
+        {
+            //if (user != null && user.UserName != null && user.Password != null)
+            //{
+                var userData = GetUser(userRequest);
+                var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
+
+                if (userData != null)
+                {
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject)
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.key));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(jwt.Issuer, jwt.Audience, claims, expires: DateTime.Now.AddMinutes(20),
+                        signingCredentials: signIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }  
+            
+            return BadRequest("Invalid Credentials");
+            
+        }
+
+        //[NonAction]
+        private  User GetUser(UserRequest userRequest)
+        {
+             var res = DemoJWT.Models.User.users.FirstOrDefault(x=> x.UserName == userRequest.UserName && x.Password == userRequest.Password);
+
+            return res;
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace DemoJWT.Models
+{
+    public  class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public static List<User> users = new()
+        {
+            new User() { UserName = "shiv" , Password="shiv"}
+        };
+    }
+
+    public class Jwt
+    {
+        public string key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Subject { get; set; }
+    }
+}
